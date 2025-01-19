976a87d90a0776d5ea225058c6a6cca001cb8f53
zoro <kr.ankit253@gmail.com>
2025-01-19 20:02:09 +0530

    add shell support in editor

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,22 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set noswapfile")
 vim.g.mapleader = " "
 
+vim.opt.clipboard = "unnamedplus"
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+  end,
+})
+
+vim.keymap.set("n", "<leader>sp", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 10)
+end)
